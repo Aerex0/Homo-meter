ae3b5f9e788a807e1bb281e103863b4a6ffae7f2
Aerex0 <suyash.ranjan.min24@itbhu.ac.in>
2026-03-11 21:28:41 +0530

    A template

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import data from './assets/questions.json' assert { type: 'json' };
+// import data from './assets/questions.json' assert { type: 'json' };
 
 import chalk from "chalk";
 import inquirer from "inquirer";
@@ -8,7 +8,6 @@ import gradient from "gradient-string";
 import figlet from "figlet";
 import chalkAnimation from 'chalk-animation'
 import { createSpinner } from "nanospinner";
-
 
 let userName;
 
@@ -25,11 +24,64 @@ async function welcome() {
 
     console.log(`
         ${chalk.bgRedBright(`Hey asshole`)}
-        Get your tiny little ass out of here,
+        Get your tiny little asshole out of here,
         Your mf asshole!!!
     `);
 }
 
 await welcome()
 
+async function askName() {
+    const answers = await inquirer.prompt({
+        name: 'username',
+        type: 'input',
+        message: 'What is your name? ',
+        default() {
+            return 'Anonymous'
+        },
+    });
 
+    userName = answers.username;
+}
+
+await askName()
+
+async function askQuestions() {
+    const answers = await inquirer.prompt({
+        name: 'question_1',
+        type: 'list',
+        message: 'What is your birthday? ',
+        choices: [
+            'a',
+            'b',
+            'c',
+            'd',
+        ],
+    });
+    return handleQuestions(answers.question_1 === 'c');
+}
+
+await askQuestions()
+
+async function handleQuestions(iscorrect) {
+    const spinner = createSpinner('checking answer...').start();
+    await sleep();
+
+    if (iscorrect) {
+        spinner.success({ text: 'Correct answer' });
+    } else {
+        spinner.error({ text: 'Incorrect answer' });
+        process.exit(1);
+    }
+}
+
+function winner() {
+    console.clear()
+
+    const msg = `Congrats ${userName}!\n You are gay`
+    figlet(msg, (err, data) => {
+        console.log(gradient.pastel.multiline(data))
+    })
+}
+
+await winner()
